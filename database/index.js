e4a851686f0da4ff5286c8ba702cac6230580d0d
jongregtom <jonthomas83@gmail.com>
2018-09-03 20:04:05 -0700

    added mongodb and POST /service and GET /servicesByZip

---
+++ database/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'db connection error:'));
+db.once('open', function() {
+  console.log('mongo connected')
+});
+
+const ServiceSchema = new Schema({
+  userId: {type: String, default: null},
+  zip: {type: String, default: null},
+  subject: {type: String, default: null},
+  text: {type: String, default: null},
+  status: {type: String, default: 'open'},
+  fulfillerId: {type: String, default: null}
+});
+
+const Service = mongoose.model('Service', ServiceSchema);
+
+module.exports = Service;
